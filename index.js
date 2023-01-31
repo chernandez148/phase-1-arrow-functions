48dfaf134418d285ebb3750fa9c2175feba9d300
Christian Hernandez <hernandezchristian94@gmail.com>
2023-01-31 09:27:52 -0800

    complete lab

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function (parameter1, parameter2) {
+  return parameter1 / parameter2;
+};
+divide(2000, 100);
+
+const square = (parameter5) => parameter5 ** 2;
+square(2);
+
+const add = (parameter3, parameter4) => {
+  return parameter3 + parameter4;
+};
+add(2000, 100);
